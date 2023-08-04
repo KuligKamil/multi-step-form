ed2af72e2fbe739f3ca54c99e87772211dfb7a8b
KuligKamil <kamil.marek.kulig@gmail.com>
2023-08-04 23:41:43 +0200

    🛼 #1 chore: work on steps

--- components/Steps.vue
+++ components/Steps.vue
@@ -4,20 +4,20 @@
 
 <template>
   <div>
-    <div class="flex justify-center items-center my-6">
-      <div class="step active">1</div>
+    <div class="flex justify-center items-center my-8">
+      <div class="step active ml-0">1</div>
       <div class="line"></div>
       <div class="step">2</div>
       <div class="line"></div>
       <div class="step">3</div>
       <div class="line"></div>
-      <div class="step">4</div>
+      <div class="step mr-0">4</div>
     </div>
   </div>
 </template>
 <style lang="postcss" scoped>
 .step {
-  @apply w-8 h-8 rounded-full bg-gray-200 mx-4 flex justify-center items-center
+  @apply w-8 h-8 flex justify-center items-center rounded-full bg-gray-200 mx-5
 }
 
 .line {
