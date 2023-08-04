3e38f7c36241324fe17c9467c18a8327864594dd
KuligKamil <kamil.marek.kulig@gmail.com>
2023-08-04 23:44:16 +0200

    💪🎨 #1 chore: work on visualization form, add component for input&label aka personal information

--- app.vue
+++ app.vue
@@ -1,32 +1,18 @@
 <script setup lang="ts">
 </script>
 <template>
-  <div class="h-screen flex flex-col justify-center grow items-center mx-auto bg-lime-50">
+  <div class="h-screen flex flex-col justify-center grow items-center mx-auto" style="background-color: #F3F3E3">
     <h1 class="text-gray-900 text-3xl font-black">Join our Community of Developers</h1>
     <p class="text-center text-gray-900 text-lg font-normal mt-4 mb-8">To join our community and participate in
       frontend challenges.<br />Please fill out the following form.</p>
     <div>
       <FormStep>
-        <div class="grid grid-cols-2 gap-1">
-          <div class="flex flex-col">
-            <label for="name" class="label">Full Name</label>
-            <input type="text" name="name" id="name" class="input" placeholder="Rocky Balboa" required />
-            <label for="email" class="label">Phone Number</label>
-            <input type="email" name="email" id="email" class="input" required />
-
-          </div>
-          <div class="flex flex-col">
-            <label for="email" class="label">Email Address </label>
-            <input type="email" name="email" id="email" class="input" required />
-            <label for="email" class="label">Portfolio/GitHub Link</label>
-            <input type="text" name="link" id="link" class="input" required />
-
-          </div>
-        </div>
-
+        <PersonalInformation name="name" type="text" placeholder="Rocky Balboa" label-text="Full Name" />
+        <PersonalInformation name="email" type="emial" placeholder="name@email.com" label-text="Email Address" />
+        <PersonalInformation name="phone" type="tel" placeholder="+91 1234567890" label-text="Phone Number" />
+        <PersonalInformation name="link" type="url" placeholder="github.com/KuligKamil"
+          label-text="Portfolio/GitHub Link" />
       </FormStep>
-
-      <span class="line"></span>
     </div>
   </div>
 </template>
@@ -36,6 +22,6 @@
 }
 
 .input {
-  @apply w-64 h-10 rounded-lg border border-gray-300
+  @apply w-64 h-10 rounded-lg border border-gray-300 p-3 pb-2.5;
 }
 </style>
--- components/FormStep.vue
+++ components/FormStep.vue
@@ -3,24 +3,32 @@
 </script>
 
 <template>
-  <div class="bg-white rounded-2xl shadow flex flex-col item-center" style="width: 640px; height: 525px">
+  <div class="bg-white rounded-2xl flex flex-col"
+    style="width: 640px; height: 525px; box-shadow: 10px 10px 0px 0px #F4EDD6">
     <Steps></Steps>
     <!-- <span class="line" style=""></span> -->
     <div class="flex flex-col items-center">
       <div class="mb-8" style="width: 580px; height: 0px; border: 0.50px #E5E7EB solid"></div>
     </div>
-    <div class="flex flex-col items-center">
-      <h2 class="text-gray-900 text-2xl font-medium mb-2">
-        Personal Information
-      </h2>
-      <p class="text-gray-500 text-sm font-normal">
-        Please provide your personal details so we can get to know you better.
-      </p>
+    <div>
+      <div class="flex flex-col ml-8 mb-5">
+        <h2 class=" text-gray-900 text-2xl font-medium mb-2">
+          Personal Information
+        </h2>
+        <p class="text-gray-500 text-sm font-normal">
+          Please provide your personal details so we can get to know you better.
+        </p>
+      </div>
+      <form class="flex flex-wrap gap-2 justify-between mx-8">
+        <slot></slot>
+        <div class="mt-8" style="width: 580px; height: 0px; border: 0.50px #E5E7EB solid"></div>
+        <div class="w-full flex flex-row-reverse">
+          <button class="w-28 h-10 px-6 py-2.5 bg-red-400 rounded-lg text-center text-white text-sm font-medium mt-4">
+            Next step
+          </button>
+        </div>
+      </form>
     </div>
-    <form class="flex flex-col items-center">
-      <slot></slot>
-      <div class="mt-10" style="width: 580px; height: 0px; border: 0.50px #E5E7EB solid"></div>
-    </form>
   </div>
 </template>
 <style lang="postcss" scoped></style>
